7e020452995cc673bd1bcf9e869754742e5ee211
moyuoss <112262034+moyuoss@users.noreply.github.com>
2024-08-27 17:23:32 +0700

    Add files via upload

--- PatrolState.cs
+++ PatrolState.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PatrolState : BaseState
 {
     public int waypointIndex;
+    public float waitTimer;
     public override void Enter()
     {
     }
     public override void Perform()
     {
         PatrolCycle();
+        if (enemy.CanSeePlayer())
+        {
+            stateMachine.ChangeState(new AttackState());
+        }
     }
     public override void Exit()
     {
@@ -19,12 +24,15 @@ public class PatrolState : BaseState
     {
         if (enemy.Agent.remainingDistance < 0.2f)
         {
-            if (waypointIndex < enemy.path.waypoints.Count - 1)
-                waypointIndex++;
-            else
-                waypointIndex = 0;
-            enemy.Agent.SetDestination(enemy.path.waypoints[waypointIndex].position);
-            
+            waitTimer += Time.deltaTime;
+            if (waitTimer > 3 )
+            {
+                if (waypointIndex < enemy.path.waypoints.Count - 1)
+                    waypointIndex++;
+                else
+                    waypointIndex = 0;
+                enemy.Agent.SetDestination(enemy.path.waypoints[waypointIndex].position);
+            }
         }
     }
 }
